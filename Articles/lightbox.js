e2423a277a98493d8d5b2611d6707917a8484890
Emils Lapins <emilslapins@gmail.com>
2018-08-05 15:34:53 +0300

    bugs

--- Articles/lightbox.js
+++ Articles/lightbox.js
@@ -1,20 +1,30 @@
+var misrc = "";
+var mihref = "";
+
 $(document).on('click', '[data-toggle="lightbox"]', function(event){
     event.preventDefault();
     $(this).ekkoLightbox();
     var a = $(this).attr("href");
     console.log($(this).attr("href"));
-    $(".mainimg").attr("src", a);
-
-});
-
-$(document).on('click', '[data-toggle="lightbox2"]', function(event){
-    event.preventDefault();
-    $(this).ekkoLightbox();
+    misrc = $(".mainimg").attr("src", a);
+    mihref = $(".mainimg").attr("href", a);
     
 
 });
 
+
+// main image
 $(document).on('click', '[data-toggle="lightbox3"]', function(event){
+    event.preventDefault();
+    $(this).ekkoLightbox();
+    console.log($(this).attr("href"));
+
+});
+
+
+
+// instagram button
+$(document).on('click', '[data-toggle="lightbox2"]', function(event){
     event.preventDefault();
     $(this).ekkoLightbox();
     
