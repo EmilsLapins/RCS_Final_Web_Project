9747c567998ac08bb63540b2c5cf84e0c526555a
Emils Lapins <emilslapins@gmail.com>
2018-08-05 13:59:11 +0300

    main image display added.

--- Articles/lightbox.js
+++ Articles/lightbox.js
@@ -1,5 +1,9 @@
 $(document).on('click', '[data-toggle="lightbox"]', function(event){
     event.preventDefault();
     $(this).ekkoLightbox();
+    var a = $(this).attr("href");
+    console.log($(this).attr("href"));
+    $(".mainimg").attr("src", a);
+
 });
 
